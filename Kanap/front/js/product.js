c00b433cd1deab5462003c7da108a757a41fb555
camille <camille-emma.d@outlook.com>
2022-12-06 13:14:11 +0100

    récéperation de la promesses/Intégration des données du produit sélectionné dans la page HTML

--- Kanap/front/js/product.js
+++ Kanap/front/js/product.js
@@ -25,3 +25,13 @@ const selectedQuantity = document.querySelector("#quantity");
 
 // Sélection du bouton Ajouter au panier
 const button = document.querySelector("#addToCart");
+
+// Fonction qui récupère les données de la promesse .then(product) pour injecter les valeurs dans le fichier Html
+let selectedProduct = (product) => {
+  // Intégration des données du produit sélectionné dans la page HTML
+  document.querySelector("head > title").textContent = product.name;
+  document.querySelector(".item__img")
+  .innerHTML += `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
+  document.querySelector("#title").textContent += product.name;
+  document.querySelector("#price").textContent += product.price;
+  document.querySelector("#description").textContent += product.description;
