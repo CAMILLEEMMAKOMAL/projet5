16ea000048dd1c85916e4bd114f697276102ee2f
camille <camille-emma.d@outlook.com>
2022-12-06 13:38:12 +0100

    Affichage du contenu du panier

--- Kanap/front/js/cart.js
+++ Kanap/front/js/cart.js
@@ -7,3 +7,9 @@ let products = [];
 // Variable qui récupère l'orderId envoyé comme réponse par le serveur lors de la requête POST
 let orderId = "";
 
+// Affichage du contenu du panier
+async function displayCart() {
+    const parser = new DOMParser();
+    const positionEmptyCart = document.getElementById("cart__items");
+    let cartArray = [];
+  
