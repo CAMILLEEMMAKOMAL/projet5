8b45ae5b0e27b081d8c82866a1dc6d25a0019209
camille <camille-emma.d@outlook.com>
2022-12-06 14:16:15 +0100

    page confirmation

---
+++ Kanap/front/js/confirmation.js
@@ -0,0 +1,10 @@
+const getProductId = () => {
+  return new URL(location.href).searchParams.get("id");
+};
+const orderId = getProductId();
+
+const cart = JSON.parse(localStorage.getItem("cart"));
+
+const idConfirmation = document.querySelector("#orderId");
+
+const btnRetourHtml = `<button id="retourAccueil"><a href="./index.html">Retour à l'accueil</a></button>`;
