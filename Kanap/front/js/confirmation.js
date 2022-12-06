97fc48be31bc7e116ccd42a185d8b9f974298fff
camille <camille-emma.d@outlook.com>
2022-12-06 14:17:23 +0100

    Affichage de l'orderId dans le DOM

--- Kanap/front/js/confirmation.js
+++ Kanap/front/js/confirmation.js
@@ -8,3 +8,16 @@ const cart = JSON.parse(localStorage.getItem("cart"));
 const idConfirmation = document.querySelector("#orderId");
 
 const btnRetourHtml = `<button id="retourAccueil"><a href="./index.html">Retour à l'accueil</a></button>`;
+
+//Affichage de l'orderId dans le DOM
+(function () {
+  idConfirmation.innerHTML = `
+  <br>
+  <strong>${orderId}</strong>. <br>
+  <br>
+  `;
+
+  idConfirmation.insertAdjacentHTML("beforeend", btnRetourHtml);
+
+  localStorage.clear();
+})();
