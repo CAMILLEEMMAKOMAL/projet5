d6ce7bea2d5102ed46054f7474adffd32366c188
camille <camille-emma.d@outlook.com>
2022-12-06 13:41:53 +0100

    Si le localstorage contient des produits

--- Kanap/front/js/cart.js
+++ Kanap/front/js/cart.js
@@ -19,3 +19,33 @@ async function displayCart() {
   } else {
     console.log("Des produits sont présents dans le panier");
   }
+
+// Si le localstorage contient des produits
+  for (i = 0; i < cart.length; i++) {
+    const product = await getProductById(cart[i].id);
+    const totalPriceItem = (product.price *= cart[i].quantity);
+    cartArray += `<article class="cart__item" data-id="${cart[i].id}" data-color="${cart[i].color}">
+                  <div class="cart__item__img">
+                      <img src="${product.imageUrl}" alt="${product.altTxt}">
+                  </div>
+                  <div class="cart__item__content">
+                      <div class="cart__item__content__description">
+                          <h2>${product.name}</h2>
+                          <p>${cart[i].color}</p>
+                          <p>Prix unitaire: ${product.price}€</p>
+                      </div>
+                      <div class="cart__item__content__settings">
+                        <div class="cart__item__content__settings__quantity">
+                            <p id="quantité">
+                              Qté : <input data-id= ${cart[i].id} data-color= ${cart[i].color} type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${cart[i].quantity}>
+                            </p>
+                            <p id="sousTotal">Prix total pour cet article: ${totalPriceItem}€</p> 
+                        </div>
+                        <div class="cart__item__content__settings__delete">
+                          <p data-id= ${cart[i].id} data-color= ${cart[i].color} class="deleteItem">Supprimer</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  </article>`;
+  }
